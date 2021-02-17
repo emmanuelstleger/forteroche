927b923a9393c57868e68667b7a262a242a2a2d9
emmanueldesaintleger <emmanuelstleger@gmail.com>
2021-02-17 12:23:35 +0100

    ajout commentaires, autoloader, router et controller

---
+++ config/Router.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\config;
+use App\src\controller\FrontController;
+use App\src\controller\ErrorController;
+use Exception;
+
+class Router
+{
+    private $frontController;
+    private $errorController;
+
+    public function __construct()
+    {
+        $this->frontController = new FrontController();
+        $this->errorController = new ErrorController();
+    }
+
+    public function run()
+    {
+        try{
+            if(isset($_GET['route']))
+            {
+                if($_GET['route'] === 'article'){
+                    $this->frontController->article($_GET['articleId']);
+                }
+                else{
+                    $this->errorController->errorNotFound();
+                }
+            }
+            else{
+                $this->frontController->home();
+            }
+        }
+        catch (Exception $e)
+        {
+            $this->errorController->errorServer();
+        }
+    }
+}
---
+++ templates/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+</head>
+
+<body>
+    <div>
+        <h1>Mon blog</h1>
+        <p>En construction</p>
+        <?php
+        while($article = $articles->fetch())
+        {
+            ?>
+            <div>
+                <h2><a href="../public/index.php?route=article&articleId=<?=htmlspecialchars($article->id);?>"><?=htmlspecialchars($article->title);?></a></h2>
+                <p><?=htmlspecialchars($article->content);?></p>
+                <p><?=htmlspecialchars($article->author);?></p>
+                <p>Créé le : <?=htmlspecialchars($article->createdAt);?></p>
+            </div>
+            <br />
+            <?php
+        }
+        $articles->closeCursor();
+        ?>
+    </div>
+</body>
+</html>
